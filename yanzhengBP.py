d04930066fb84b9d7def2a12abbd20a2f8bac653
hongbo Sun <1075103615@qq.com>
2018-01-25 11:32:04 +0800

    Add files via upload

---
+++ yanzhengBP.py
@@ -0,0 +1,118 @@
+'''#单层神经网络
+import numpy as np
+
+# sigmoid function
+def nonlin(x,deriv=False):
+    if(deriv==True):
+        return x*(1-x)
+    return 1/(1+np.exp(-x))
+    
+# input dataset
+X = np.array([  [0,0,1],
+                [0,1,1],
+                [1,0,1],
+                [1,1,1] ])
+    
+# output dataset            
+y = np.array([[0,0,1,1]]).T
+
+# seed random numbers to make calculation
+# deterministic (just a good practice)
+np.random.seed(1)
+
+# initialize weights randomly with mean 0
+syn0 = 2*np.random.random((3,1)) - 1
+l0 = X
+l1 = nonlin(np.dot(l0,syn0))
+print l1
+print 
+for iter in xrange(100000):
+
+    # forward propagation
+    l0 = X
+    l1 = nonlin(np.dot(l0,syn0))
+
+    # how much did we miss?
+    l1_error = y - l1
+
+    # multiply how much we missed by the 
+    # slope of the sigmoid at the values in l1
+    l1_delta = l1_error * nonlin(l1,True)
+
+    # update weights
+    syn0 += np.dot(l0.T,l1_delta/4)#求各个参数变化量的均值
+
+print "Output After Training:"
+print l1
+'''
+#双层神经网络(可引申到多层)
+import numpy as np
+
+def nonlin(x,deriv=False):
+	if(deriv==True):
+	    return x*(1-x)
+
+	return 1/(1+np.exp(-x))
+    
+X = np.array([[0,0,1],
+            [0,1,1],
+            [1,0,1],
+            [1,1,1]])
+                
+y = np.array([[0],
+			[1],
+			[1],
+			[0]])
+
+np.random.seed(1)
+
+# randomly initialize our weights with mean 0
+syn0 = 2*np.random.random((3,4)) - 1
+syn1 = 2*np.random.random((4,1)) - 1
+buchang=1
+for j in xrange(100000):
+
+	# Feed forward through layers 0, 1, and 2
+    l0 = X
+    l1 = nonlin(np.dot(l0,syn0))
+    l2 = nonlin(np.dot(l1,syn1))
+    #print l2
+    #print
+    # how much did we miss the target value?
+    l2_error = y - l2
+    
+    if (j% 10000) == 0:
+        print "Error:" + str(np.mean(np.abs(l2_error)*np.abs(l2_error)))
+        
+    # in what direction is the target value?
+    # were we really sure? if so, don't change too much.
+    l2_delta = l2_error*nonlin(l2,True)
+   # how much did each l1 value contribute to the l2 error (according to the weights)?
+    l1_error = l2_delta.dot(syn1.T)
+    
+    # in what direction is the target l1?
+    # were we really sure? if so, don't change too much.
+    l1_delta = l1_error * nonlin(l1,deriv=True)
+
+#方向全部算好后进行更新参数
+    syn0 += buchang*(l0.T.dot(l1_delta))/4.0
+    syn1 += buchang*(l1.T.dot(l2_delta))/4.0     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
